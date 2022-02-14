45cba124cc21335028b2ebc29ee84510a0384a5d
Kittanan(TA) <ta.kittanan@gmail.com>
2022-02-14 17:03:56 +0700

    Add files via upload

---
+++ CVKeygen/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿
+namespace CVKeygen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_hwid = new System.Windows.Forms.TextBox();
+            this.txt_lifetime = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn_createkey = new System.Windows.Forms.Button();
+            this.txt_output = new System.Windows.Forms.TextBox();
+            this.txt_decrypt = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "UUID";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txt_hwid
+            // 
+            this.txt_hwid.Location = new System.Drawing.Point(16, 32);
+            this.txt_hwid.Name = "txt_hwid";
+            this.txt_hwid.Size = new System.Drawing.Size(326, 26);
+            this.txt_hwid.TabIndex = 1;
+            this.txt_hwid.Text = "0";
+            this.txt_hwid.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txt_lifetime
+            // 
+            this.txt_lifetime.Location = new System.Drawing.Point(16, 86);
+            this.txt_lifetime.Name = "txt_lifetime";
+            this.txt_lifetime.Size = new System.Drawing.Size(326, 26);
+            this.txt_lifetime.TabIndex = 3;
+            this.txt_lifetime.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(111, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "LifeTime (Day)";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // btn_createkey
+            // 
+            this.btn_createkey.Location = new System.Drawing.Point(16, 118);
+            this.btn_createkey.Name = "btn_createkey";
+            this.btn_createkey.Size = new System.Drawing.Size(326, 33);
+            this.btn_createkey.TabIndex = 4;
+            this.btn_createkey.Text = "Create License Key";
+            this.btn_createkey.UseVisualStyleBackColor = true;
+            this.btn_createkey.Click += new System.EventHandler(this.btn_createkey_Click);
+            // 
+            // txt_output
+            // 
+            this.txt_output.Location = new System.Drawing.Point(16, 157);
+            this.txt_output.Multiline = true;
+            this.txt_output.Name = "txt_output";
+            this.txt_output.Size = new System.Drawing.Size(326, 89);
+            this.txt_output.TabIndex = 5;
+            // 
+            // txt_decrypt
+            // 
+            this.txt_decrypt.Location = new System.Drawing.Point(16, 252);
+            this.txt_decrypt.Multiline = true;
+            this.txt_decrypt.Name = "txt_decrypt";
+            this.txt_decrypt.Size = new System.Drawing.Size(326, 89);
+            this.txt_decrypt.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(359, 248);
+            this.Controls.Add(this.txt_decrypt);
+            this.Controls.Add(this.txt_output);
+            this.Controls.Add(this.btn_createkey);
+            this.Controls.Add(this.txt_lifetime);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txt_hwid);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CVPro License Key Generator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_hwid;
+        private System.Windows.Forms.TextBox txt_lifetime;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btn_createkey;
+        private System.Windows.Forms.TextBox txt_output;
+        private System.Windows.Forms.TextBox txt_decrypt;
+    }
+}
+
---
+++ CVKeygen/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Management;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CVKeygen
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ManagementObjectSearcher mos = new ManagementObjectSearcher("SELECT * FROM Win32_ComputerSystemProduct");
+            ManagementObjectCollection moc = mos.Get();
+            string motherBoard = "";
+            foreach (ManagementObject mo in moc)
+            {
+                motherBoard = (string)mo["UUID"];
+            }
+            txt_hwid.Text = motherBoard;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_createkey_Click(object sender, EventArgs e)
+        {
+            var addedDate = DateTime.Now.Date.AddDays(Convert.ToDouble(txt_lifetime.Text));
+            var key = txt_hwid.Text + "|" + addedDate.ToShortDateString();
+            txt_output.Text = Encrypt(key, "P@$$W0RD!@#$%123456");
+            //txt_decrypt.Text = Decrypt(txt_output.Text, "P@$$W0RD!@#$%123456");
+        }
+
+        public static string Encrypt(string PlainText, string Password,
+              string Salt = "h00Sch~|^:><i3]HLp+u`@xcc|r4KUeG#ve%OCr`U2TP]J[y!;I9.&9_VS|u*G", string HashAlgorithm = "SHA256",
+              int PasswordIterations = 2, string InitialVector = "CodeVirtual*12!+",
+              int KeySize = 256)
+        {
+            byte[] InitialVectorBytes = Encoding.ASCII.GetBytes(InitialVector);
+            byte[] SaltValueBytes = Encoding.ASCII.GetBytes(Salt);
+            byte[] PlainTextBytes = Encoding.UTF8.GetBytes(PlainText);
+            PasswordDeriveBytes DerivedPassword = new PasswordDeriveBytes(Password, SaltValueBytes, HashAlgorithm, PasswordIterations);
+            byte[] KeyBytes = DerivedPassword.GetBytes(KeySize / 8);
+            RijndaelManaged SymmetricKey = new RijndaelManaged();
+            SymmetricKey.Mode = CipherMode.CBC;
+            byte[] CipherTextBytes = null;
+            using (ICryptoTransform Encryptor = SymmetricKey.CreateEncryptor(KeyBytes, InitialVectorBytes))
+            {
+                using (MemoryStream MemStream = new MemoryStream())
+                {
+                    using (CryptoStream CryptoStream = new CryptoStream(MemStream, Encryptor, CryptoStreamMode.Write))
+                    {
+                        CryptoStream.Write(PlainTextBytes, 0, PlainTextBytes.Length);
+                        CryptoStream.FlushFinalBlock();
+                        CipherTextBytes = MemStream.ToArray();
+                        MemStream.Close();
+                        CryptoStream.Close();
+                    }
+                }
+            }
+            SymmetricKey.Clear();
+            return Convert.ToBase64String(CipherTextBytes);
+        }
+
+        public static string Decrypt(string CipherText, string Password,
+              string Salt = "h00Sch~|^:><i3]HLp+u`@xcc|r4KUeG#ve%OCr`U2TP]J[y!;I9.&9_VS|u*G", string HashAlgorithm = "SHA256",
+              int PasswordIterations = 2, string InitialVector = "CodeVirtual*12!+",
+              int KeySize = 256)
+        {
+            if (string.IsNullOrEmpty(CipherText))
+                return "";
+            byte[] InitialVectorBytes = Encoding.ASCII.GetBytes(InitialVector);
+            byte[] SaltValueBytes = Encoding.ASCII.GetBytes(Salt);
+            byte[] CipherTextBytes = Convert.FromBase64String(CipherText);
+            PasswordDeriveBytes DerivedPassword = new PasswordDeriveBytes(Password, SaltValueBytes, HashAlgorithm, PasswordIterations);
+            byte[] KeyBytes = DerivedPassword.GetBytes(KeySize / 8);
+            RijndaelManaged SymmetricKey = new RijndaelManaged();
+            SymmetricKey.Mode = CipherMode.CBC;
+            byte[] PlainTextBytes = new byte[CipherTextBytes.Length];
+            int ByteCount = 0;
+            using (ICryptoTransform Decryptor = SymmetricKey.CreateDecryptor(KeyBytes, InitialVectorBytes))
+            {
+                using (MemoryStream MemStream = new MemoryStream(CipherTextBytes))
+                {
+                    using (CryptoStream CryptoStream = new CryptoStream(MemStream, Decryptor, CryptoStreamMode.Read))
+                    {
+
+                        ByteCount = CryptoStream.Read(PlainTextBytes, 0, PlainTextBytes.Length);
+                        MemStream.Close();
+                        CryptoStream.Close();
+                    }
+                }
+            }
+            SymmetricKey.Clear();
+            return Encoding.UTF8.GetString(PlainTextBytes, 0, ByteCount);
+        }
+    }
+}
